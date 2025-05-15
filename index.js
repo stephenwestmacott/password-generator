9c87366e6cd7a9dac0edbaecce6aed5d2ca7f3e0
stephenwestmacott <stephenhwestmacott@gmail.com>
2025-05-15 16:30:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let passOne = ""
+let passTwo = ""
+let passElOne = document.getElementById("pass-one")
+let passElTwo = document.getElementById("pass-two")
+
+
+function generatePasswordOne() {
+    passOne = ""
+    for (let i = 0; i < 15; i++){
+        randomIndex = Math.floor(Math.random() * characters.length)
+        passOne += characters[randomIndex]
+    }
+}
+
+function generatePasswordTwo() {
+    passTwo = ""
+    for (let i = 0; i < 15; i++){
+        randomIndex = Math.floor(Math.random() * characters.length)
+        passTwo += characters[randomIndex]
+    }
+}
+
+function generatePasswords() {
+    generatePasswordOne()
+    generatePasswordTwo()
+    passElOne.textContent = passOne
+    passElTwo.textContent = passTwo
+}
